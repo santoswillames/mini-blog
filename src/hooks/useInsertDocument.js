1ee538b2d812467803066e3b67ea8d38cf93c51f
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2022-09-28 16:40:51 -0300

    page create post

---
+++ src/hooks/useInsertDocument.js
@@ -0,0 +1,73 @@
+import { useState, useEffect, useReducer } from "react";
+import { db } from "../firebase/config";
+import { collection, addDoc, Timestamp } from "firebase/firestore";
+
+const initialState = {
+  loading: null,
+  error: null,
+};
+
+const insertReducer = (state, action) => {
+  switch (action.type) {
+    case "LOADING":
+      return { loading: true, error: null };
+    case "INSERTED_DOC":
+      return { loading: false, error: null };
+    case "ERROR":
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const useInsertDocument = (docCollection) => {
+  const [response, dispatch] = useReducer(insertReducer, initialState);
+
+  //deal with memory leak
+  const [cancelled, setCancelled] = useState(false);
+
+  const checkCancelBeforeDispatch = (action) => {
+    if (!cancelled) {
+      dispatch(action);
+    }
+  };
+
+  // Função que inseri o Post
+  const insertDocument = async (document) => {
+    checkCancelBeforeDispatch({
+      type: "LOADING",
+    });
+
+    try {
+      //Pegando o documento que vai ser inserido, usando o spread operator
+      const newDocument = { ...document, createdAt: Timestamp.now() };
+
+      //Função com o resultado da inserção
+      const insertedDocument = await addDoc(
+        //Faz a procurada coleção no db e se retornar true faz a inserção
+        collection(db, docCollection),
+        newDocument
+      );
+
+      //Como temos uma ação no sistema acontecendo temos que dar o dispatch
+      checkCancelBeforeDispatch({
+        type: "INSERTED_DOC",
+        payload: insertedDocument,
+      });
+    } catch (error) {
+      checkCancelBeforeDispatch({
+        type: "ERROR",
+        payload: error.message,
+      });
+    }
+    useEffect(() => {
+      //Encerrando o componente
+      return () => setCancelled(true);
+    }, []);
+  };
+
+  return {
+    insertDocument,
+    response,
+  };
+};
--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,9 +1,99 @@
 import styles from "./CreatePost.module.css";
 
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuthValue } from "../../contexts/AuthContext";
+import { useInsertDocument } from "../../hooks/useInsertDocument";
+
 const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState("");
+  const [body, setBody] = useState("");
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState("");
+
+  const { insertDocument, response } = useInsertDocument("posts");
+  const { user } = useAuthValue();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormError("");
+
+    // Validate image URL
+
+    //Criar o array de tags
+
+    //Checar todos os valores
+
+    insertDocument({
+      title,
+      image,
+      body,
+      tags,
+      uid: user.uid,
+      createdBy: user.displayName,
+    });
+
+    // Redirect to home page
+  };
+
   return (
-    <div>
-      <h1>Create Post</h1>
+    <div className={styles.create_post}>
+      <h1>Criar Post</h1>
+      <p>Escreva sobre o que você quiser e compartilhe o seu conhecimento</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Título:</span>
+          <input
+            type="text"
+            name="title"
+            required
+            placeholder="Pense em um bom título..."
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>URL da imagem:</span>
+          <input
+            type="text"
+            name="image"
+            required
+            placeholder="Insira uma imagem que representa o seu post"
+            onChange={(e) => setImage(e.target.value)}
+            value={image}
+          />
+        </label>
+        <label>
+          <span>Conteúdo</span>
+          <textarea
+            name="body"
+            required
+            placeholder="Insira o conteúdo do post"
+            onChange={(e) => setBody(e.target.value)}
+            value={body}
+          ></textarea>
+        </label>
+        <label>
+          <span>Tags:</span>
+          <input
+            type="text"
+            name="tags"
+            required
+            placeholder="Insira as tags separadas por vírgula"
+            onChange={(e) => setTags(e.target.value)}
+            value={tags}
+          />
+        </label>
+
+        {!response.loading && <button className="btn">Cadastrar</button>}
+        {response.loading && (
+          <button className="btn" disabled>
+            Aguarde...
+          </button>
+        )}
+        {response.error && <p className="error">{response.error}</p>}
+      </form>
     </div>
   );
 };
